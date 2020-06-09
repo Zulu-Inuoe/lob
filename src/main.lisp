55f702efb149a2998e769394b78fe918bffb098a
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-06-09 14:42:43 -0400

    Clean up arg parsing a smidge

--- src/main.lisp
+++ src/main.lisp
@@ -14,11 +14,41 @@
                                  (subseq outstring 0 (position #\Return outstring))
                                  outstring)))))
 
+(defmacro string-case (value &body clauses)
+  (let ((value-sym (gensym "VALUE")))
+    `(let ((,value-sym ,value))
+       (cond
+         ,@(mapcar (lambda (c)
+                     (destructuring-bind (clause . body) c
+                       (cond
+                         ((eq clause t)
+                          `(t ,@body))
+                         ((atom clause)
+                          `((string= ,value-sym ,clause) ,@body))
+                         (t
+                          `((or ,@(mapcar (lambda (x) `(string= ,value-sym ,x)) clause))
+                            ,@body)))))
+                   clauses)))))
+
+(defun parse-entry-name (name)
+  (let* ((colon (position #\: name))
+         (package-name (if colon (subseq name 0 colon)))
+         (name-start (if colon
+                         (let ((second-colon (position #\: name :start (1+ colon))))
+                           (if second-colon
+                               (prog1 (1+ second-colon)
+                                 (when (position #\: name :start (1+ second-colon))
+                                   (error "invalid entry point name - too many colons: ~A" name)))
+                               (1+ colon)))
+                         0))
+         (symbol-name (subseq name name-start)))
+    (values package-name symbol-name)))
+
 (defun cl-user::main (argv)
-  (flet ((lose (str)
-               (format *error-output* "lob: ~A~%" str)
-               (finish-output *standard-output*)
-               (finish-output *error-output*)
+  (flet ((lose (str &rest args)
+           (apply #'format *error-output* str args)
+           (finish-output *standard-output*)
+           (finish-output *error-output*)
            (return-from cl-user::main 1)))
     (handler-case
         (loop
@@ -27,82 +57,55 @@
           :with loaded-things := ()
           :with output-path := nil
           :with gui := nil
-          :with cell := (cdr argv)
           :with include-directories := ()
           :with debug-build := nil
           :with verbose := nil
+          :with cell := (cdr argv)
           :for elt := (car cell)
           :while cell
           :do
-             (cond
-               ((string= elt "--help")
-                (format t "usage: lob [--version] [--help] [-v | --verbose]
+             (flet ((take-arg ()
+                      (setf cell (cdr cell))
+                      (unless cell
+                        (lose "lob: missing argument for ~A" elt))
+                      (car cell)))
+               (string-case elt
+                 ("--help"
+                  (format t "usage: lob [--version] [--help] [-v | --verbose]
            [-I <path>] [-o <path>] [-g] [-d]
-           [-e | --entry-point [<package>:[:]]<name>] [-s <system-name>]
+           [-e | --entry-point [<package>:[:]]<name>] [-l <system-name>]
            <path>...~2%")
-                (return-from cl-user::main 0))
-               ((string= elt "--version")
-                (format t "1.0.0~%")
-                (return-from cl-user::main 0))
-               ((or (string= elt "-e")
-                    (string= elt "--entry-point"))
-                (let ((next (cdr cell)))
-                  (unless next
-                    (lose (format nil "missing argument for ~A" elt)))
-                  (let* ((name (car next))
-                         (colon (position #\: name))
-                         (package-name (if colon (subseq name 0 colon)))
-                         (name-start (if colon
-                                         (let ((second-colon (position #\: name :start (1+ colon))))
-                                           (if second-colon
-                                               (prog1 (1+ second-colon)
-                                                 (when (position #\: name :start (1+ second-colon))
-                                                   (lose (format nil "invalid entry point name - too many colons: ~A" name))))
-                                               (1+ colon)))
-                                         0))
-                         (symbol-name (subseq name name-start)))
-                    (setf toplevel-package-name package-name
-                          toplevel-symbol-name symbol-name
-                          cell (cdr next)))))
-               ((string= elt "-g")
-                (setf gui t
-                      cell (cdr cell)))
-               ((string= elt "-o")
-                (let ((next (cdr cell)))
-                  (unless next
-                    (lose (format nil "missing argument for ~A" elt)))
-                  (setf output-path (car next)
-                        cell (cdr next))))
-               ((string= elt "-s")
-                (let ((next (cdr cell)))
-                  (unless next
-                    (lose (format nil "missing argument for ~A" elt)))
-                  (push (make-instance 'system-name :name (car next)) loaded-things)
-                  (setf cell (cdr next))))
-               ((string= elt "-I")
-                (let ((next (cdr cell)))
-                  (unless next
-                    (lose (format nil "missing argument for ~A" elt)))
-                  (push (truename (uiop:ensure-directory-pathname (car next))) include-directories)
-                  (setf cell (cdr next))))
-               ((string= elt "-d")
-                (setf debug-build t
-                      cell (cdr cell)))
-               ((or (string= elt "-v")
-                    (string= elt "--verbose"))
-                (setf verbose t
-                      cell (cdr cell)))
-               ((or (string= elt "-" :end1 (min 1 (length elt)))
-                    (string= elt "--" :end1 (min 2 (length elt))))
-                (lose (format nil "unknown option: ~A" elt)))
-               (t
-                (push elt loaded-things)
-                (setf cell (cdr cell))))
+                  (return-from cl-user::main 0))
+                 ("--version"
+                  (format t "1.0.0~%")
+                  (return-from cl-user::main 0))
+                 (("-e" "--entry-point")
+                  (setf (values toplevel-package-name toplevel-symbol-name)
+                        (parse-entry-name (take-arg))))
+                 ("-g"
+                  (setf gui t))
+                 ("-o"
+                  (setf output-path (take-arg)))
+                 ("-l"
+                  (push (make-instance 'system-name :name (take-arg)) loaded-things))
+                 ("-I"
+                  (push (truename (uiop:ensure-directory-pathname (take-arg))) include-directories))
+                 ("-d"
+                  (setf debug-build t))
+                 (("-v" "--verbose")
+                  (setf verbose t))
+                 (t
+                  (when (or (string= elt "-" :end1 (min 1 (length elt)))
+                            (string= elt "--" :end1 (min 2 (length elt))))
+                    (lose "lob: unknown option: ~A" elt))
+                  (push elt loaded-things)))
+
+               (setf cell (cdr cell)))
           :finally
              (setf include-directories (nreverse include-directories)
                    loaded-things (nreverse loaded-things))
              (unless loaded-things
-               (lose "no input files"))
+               (lose "lob: no input files"))
              (return (let ((*lob-stdout* (if verbose *standard-output* (make-broadcast-stream))))
                        (build :image (executable-find "sbcl")
                               :gui gui
@@ -113,4 +116,4 @@
                               :debug-build debug-build
                               :additional-source-registry include-directories))))
       (error (e)
-        (lose (format nil "~A" e))))))
+        (lose "lob: ~A" e)))))
